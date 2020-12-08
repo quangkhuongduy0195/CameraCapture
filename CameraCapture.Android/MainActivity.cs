6fe294bfdda06574d51260bcd5786dfa3430d921
Huy Pham <huyph@runsystem.net>
2020-12-08 16:15:25 +0700

    Permission
    Fix show permission for user.

--- CameraCapture.Android/MainActivity.cs
+++ CameraCapture.Android/MainActivity.cs
@@ -1,23 +1,15 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using Plugin.CurrentActivity;
-using Android.Support.V4.Content;
-using Android;
-using Android.Support.V4.App;
-using Android.Content;
-using Xamarin.Essentials;
 
 namespace CameraCapture.Droid
 {
     [Activity(Label = "CameraCapture", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        //CameraViewRendererAndroid _cameraViewRendererAndroid;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -29,8 +21,6 @@ namespace CameraCapture.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-            _ = CreateOrGetStorageAsync();
-            CheckPermission();
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -38,67 +28,6 @@ namespace CameraCapture.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        public bool FirstTime = false;
-        async System.Threading.Tasks.Task CreateOrGetStorageAsync ()
-        {
-            var firstTime = await SecureStorage.GetAsync("firsttime");
-            if (firstTime != null)
-                FirstTime = bool.Parse(firstTime);
-
-            if (FirstTime == false)
-            {
-                await SecureStorage.SetAsync("firsttime", "true");
-                return;
-            }
-        }
-
-        public void ShowDialog (string message)
-        {
-            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-            dialog.SetTitle("Thông báo");
-            dialog.SetMessage(message);
-            dialog.SetPositiveButton("Huỷ", (s, a) =>
-            {
-                dialog.Dispose();
-            });
-            dialog.SetNegativeButton("Cài đặt", (s, a) =>
-            {
-                var context = Android.App.Application.Context;
-                Intent i = new Android.Content.Intent(Android.Provider.Settings.ActionApplicationDetailsSettings);
-                Android.Net.Uri uri = Android.Net.Uri.FromParts("package", this.PackageName, null);
-                i.SetData(uri);
-                context.StartActivity(i);
-            });
-            AlertDialog createDialogCamera = dialog.Create();
-            createDialogCamera.Show();
-        }
-
-        public void CheckPermission()
-        {
-            if (int.Parse(Build.VERSION.Sdk.ToString()) >= 23)
-            {
-                var permCamera = ContextCompat.CheckSelfPermission(this, "android.permission.CAMERA");
-                var permStorage = ContextCompat.CheckSelfPermission(this, "android.permission.READ_EXTERNAL_STORAGE");
-                string messCamera = "Ứng dụng cần sử dụng máy ảnh. Vui lòng vào cài đặt để cấp quyền.";
-                string messStorage = "Ứng dụng cần sử dụng bộ nhớ. Vui lòng vào cài đặt để cấp quyền.";
-                if (permCamera != Android.Content.PM.Permission.Granted
-                    || permStorage != Android.Content.PM.Permission.Granted)
-                {
-                    ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.Camera, Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage}, 114);
-                    
-                    if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.Camera) == false && permCamera != Android.Content.PM.Permission.Granted && FirstTime != false)
-                    {
-                        ShowDialog(messCamera);
-                    }
-
-                    if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.ReadExternalStorage) == false && permStorage != Android.Content.PM.Permission.Granted && FirstTime != false)
-                    {
-                        ShowDialog(messStorage);
-                    }
-                }
-            }
         }
     }
 }
--- CameraCapture.Android/Renderers/Camera/CameraViewRendererAndroid.cs
+++ CameraCapture.Android/Renderers/Camera/CameraViewRendererAndroid.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
+using Android;
 using Android.Content;
 using Android.Content.PM;
 using Android.Graphics;
 using Android.Hardware.Camera2;
 using Android.Media;
 using Android.Provider;
+using Android.Runtime;
+using Android.Support.V4.App;
 using Android.Util;
 using CameraCapture.Droid.Renderers.Camera;
 using CameraCapture.Renderers.Camera;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Plugin.CurrentActivity;
+using System.ComponentModel;
 
 [assembly: ExportRenderer(typeof(CameraView), typeof(CameraViewRendererAndroid))]
+[assembly: Dependency(typeof(CameraViewRendererAndroid))]
 namespace CameraCapture.Droid.Renderers.Camera
 {
     public class CameraViewRendererAndroid : ViewRenderer<CameraView, CameraDroid>
@@ -30,29 +37,21 @@ namespace CameraCapture.Droid.Renderers.Camera
         protected async override void OnElementChanged(ElementChangedEventArgs<CameraView> e)
         {
             base.OnElementChanged(e);
-            var permissionCamera = await Permissions.CheckStatusAsync<Permissions.Camera>();
-            switch (permissionCamera)
-            {
-                case PermissionStatus.Unknown:
-                case PermissionStatus.Denied:
-                case PermissionStatus.Disabled:
-                    break;
-                case PermissionStatus.Granted:
-                    _camera = new CameraDroid(Context);
-                    SetNativeControl(_camera);
+            await CheckCameraPermission();
+        }
 
-                    if (e.NewElement != null && _camera != null)
-                    {
-                        //e.NewElement.CameraClick = new Command(() => TakePicture());
-                        _currentElement = e.NewElement;
-                        _camera.SetCameraOption(_currentElement.CameraOption);
-                        _camera.Photo += OnPhoto;
-                        _currentElement._captureAction = TakePicture;
-                    }
-                    break;
-                case PermissionStatus.Restricted:
-                    break;
-            } 
+        private void AddCamera()
+        {
+            _camera = new CameraDroid(Context);
+            SetNativeControl(_camera);
+
+            if (Element != null && _camera != null)
+            {
+                _currentElement = Element;
+                _camera.SetCameraOption(_currentElement.CameraOption);
+                _camera.Photo += OnPhoto;
+                _currentElement._captureAction = TakePicture;
+            }
         }
 
         public void TakePicture()
@@ -62,7 +61,8 @@ namespace CameraCapture.Droid.Renderers.Camera
 
         private void OnPhoto(object sender, byte[] imgSource)
         {
-            Device.BeginInvokeOnMainThread(() => {
+            Device.BeginInvokeOnMainThread(() =>
+            {
                 var stream = new MemoryStream(imgSource);
                 var imageSource = ImageSource.FromStream(() => stream);
                 _currentElement.HandleDidFinishProcessingPhoto(imageSource);
@@ -71,15 +71,130 @@ namespace CameraCapture.Droid.Renderers.Camera
 
         protected override void Dispose(bool disposing)
         {
-            if(_camera != null)
+            if (_camera != null)
             {
                 _camera.Photo -= OnPhoto;
             }
-            if(_currentElement != null)
+            if (_currentElement != null)
             {
                 _currentElement._captureAction = null;
             }
             base.Dispose(disposing);
+        }
+
+        public async Task CheckCameraPermission()
+        {
+            var rationale = ActivityCompat.ShouldShowRequestPermissionRationale(CrossCurrentActivity.Current.Activity, Manifest.Permission.Camera);
+            var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+            await CreateOrGetCameraAsync();
+            if (rationale == true)
+                SetfirsttimeCamera();
+            switch (status)
+            {
+                case PermissionStatus.Unknown:
+                case PermissionStatus.Denied:
+                case PermissionStatus.Disabled:
+                    if (rationale == false && FirstTimeCamera)
+                    {
+                        var rs = await App.Current.MainPage.DisplayAlert("Thông báo", "Ứng dụng cần sử dụng máy ảnh. Vui lòng vào cài đặt để cấp quyền", "Cài đặt", "Huỷ");
+                        if (rs)
+                        {
+                            Intent i = new Android.Content.Intent(Android.Provider.Settings.ActionApplicationDetailsSettings);
+                            Android.Net.Uri uri = Android.Net.Uri.FromParts("package", _context.PackageName, null);
+                            i.SetData(uri);
+                            _context.StartActivity(i);
+                        }
+                    }
+                    await RequestCameraPermission();
+                    break;
+                case PermissionStatus.Granted:
+                    AddCamera();
+                    await CheckStogarePermission();
+                    break;
+                case PermissionStatus.Restricted:
+                    break;
+            }
+        }
+
+        private async Task RequestCameraPermission()
+        {
+            await Permissions.RequestAsync<Permissions.Camera>();
+            await CheckCameraPermission();
+        }
+
+        private async Task CheckStogarePermission()
+        {
+            var rationale = ActivityCompat.ShouldShowRequestPermissionRationale(CrossCurrentActivity.Current.Activity, Manifest.Permission.ReadExternalStorage);
+            var status = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+            await CreateOrGetStorageAsync();
+            if (rationale == true)
+                SetfirsttimeStorage();
+            switch (status)
+            {
+                case PermissionStatus.Unknown:
+                case PermissionStatus.Denied:
+                case PermissionStatus.Disabled:
+                    // Check user choose "Don't show again"
+                    if (rationale == false && FirstTimeStorage)
+                    {
+                        var rs = await App.Current.MainPage.DisplayAlert("", "Ứng dụng cần sử dụng bộ nhớ. Vui lòng vào cài đặt để cấp quyền", "Cài đặt", "Close");
+                        if (rs)
+                        {
+                            Intent i = new Android.Content.Intent(Android.Provider.Settings.ActionApplicationDetailsSettings);
+                            Android.Net.Uri uri = Android.Net.Uri.FromParts("package", _context.PackageName, null);
+                            i.SetData(uri);
+                            _context.StartActivity(i);
+                        }
+                    }
+                    await RequestStogarePermission();
+                    break;
+                case PermissionStatus.Granted:
+                    SetfirsttimeCamera();
+                    break;
+                case PermissionStatus.Restricted:
+                    break;
+            }
+        }
+        private async Task RequestStogarePermission()
+        {
+            await Permissions.RequestAsync<Permissions.StorageRead>();
+            await CheckStogarePermission();
+        }
+
+        public bool FirstTimeCamera = false;
+        async System.Threading.Tasks.Task CreateOrGetCameraAsync()
+        {
+            var firstTime = await SecureStorage.GetAsync("firsttimeCamera");
+            if (firstTime != null)
+                FirstTimeCamera = bool.Parse(firstTime);
+
+        }
+
+        async void SetfirsttimeCamera()
+        {
+            if (FirstTimeCamera == false)
+            {
+                await SecureStorage.SetAsync("firsttimeCamera", "true");
+                return;
+            }
+        }
+
+        public bool FirstTimeStorage = false;
+        async System.Threading.Tasks.Task CreateOrGetStorageAsync()
+        {
+            var firstTime = await SecureStorage.GetAsync("firsttimeStorage");
+            if (firstTime != null)
+                FirstTimeStorage = bool.Parse(firstTime);
+        }
+
+        async void SetfirsttimeStorage()
+        {
+            if (FirstTimeStorage == false)
+            {
+                await SecureStorage.SetAsync("firsttimeStorage", "true");
+                return;
+            }
+
         }
     }
 
